57322298b2e0dc236fc20b6b4e9d89b82c9281c9
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-04-21 15:55:34 +0200

    oui

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-/**
- * Description
- */
-
+void execmd(char **argv);
+char *get_location(char *command);
+int stat(const char *pathname, struct stat *statbuf);
