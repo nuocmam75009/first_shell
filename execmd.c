a2fbe441495a3bea6c4be87e9b68807d8493e751
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-04-21 18:30:41 +0200

    modified the execmd file

--- execmd.c
+++ execmd.c
@@ -2,19 +2,46 @@
 
 void execmd(char **argv){
 	char *command = NULL, *actual_command = NULL;
+	pid_t pid = fork(); // tout en dessous éxécuté deux fois
+	int status;
 
-	if (argv){
-		/* get the command */
-		command = argv[0];
+	if (pid == -1)
+	{
+		perror("Error:");
+		free(argv);
+		return (1);
+	}
 
-		/* generate the path to this command before passing it to execve */
-		actual_command = get_location(command);
-
-		/* execute the actual command with execve */
-		if (execve(actual_command, argv, NULL) == -1)
+	if (pid == 0)
+	{
+		if (argv)
 		{
-			perror("Error:");
+			/* get the command */
+			command = argv[0];
+
+			/* generate the path to this command before passing it to execve */
+			actual_command = get_location(command);
+
+			/* execute the actual command with execve */
+			if (execve(actual_command, argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
 		}
 	}
 
+	else // pid > 0 donc c'est le code du parent
+	{
+		wait (&status); //wait for child to finish
+
+		if (WIFEXITED(status)) // check if child process exited normally
+		{
+			if (WEXITSTATUS(status) != 0) //check status exit of child process
+			{
+				free(argv);
+				exit(2);
+			}
+		}
+
+	}
 }
